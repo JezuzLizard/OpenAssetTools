2b1d9c3051dd6e51c95b802caf6a0e4b3417f6c0
Laupetin <jan@laupetin.net>
2019-12-23 17:03:16 +0100

    ZoneCode: Fix Commands for Material, GfxImage and Techset

--- src/ZoneCommon/Game/T6/T6_Assets.h
+++ src/ZoneCommon/Game/T6/T6_Assets.h
@@ -2665,7 +2665,7 @@ namespace T6
     };
 
     /* 1603 */
-    struct MaterialConstantDef
+    struct __declspec(align(16)) MaterialConstantDef
     {
         unsigned int nameHash;
         char name[12];
